d0faf9cf2f4ac41225005a2d0d03b4a7b4012c97
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-10-08 12:23:43 -0300

    feat: add Linked list data structure

---
+++ linked_list/list.c
@@ -0,0 +1,84 @@
+#include "list.h"
+
+struct list {
+  int value;
+  struct list *next;
+};
+
+int lst_isEmpty(List *l) {
+  if (l == NULL) {
+    printf("List is empty!\n");
+    return 1;
+  }
+  return 0;
+}
+
+List *lst_create() {
+  return NULL;
+}
+
+List *lst_insert(List *l, int v) {
+  List *new = (List *)malloc(sizeof(List));
+  if (new == NULL) {
+    printf("[ERRO] Memory allocation Failed!");
+    exit(1);
+  }
+
+  new->value = v;
+  new->next = NULL;
+
+  if (l == NULL) {
+    l = new;
+    return l;
+  }
+
+  List *current = l;
+
+  while(current->next != NULL) {
+    current = current->next;
+  }
+  current->next = new;
+  return l;
+}
+
+
+
+void lst_print(List *l) {
+  if (lst_isEmpty(l)) {
+    return;
+  }
+  
+  List current = *l;
+  printf("%d", current.value);
+  while (current.next != NULL) {
+    current = *(current.next);
+    printf(" -> %d", current.value);
+  }
+  printf("\n");
+}
+
+void lst_free(List *l) {
+  List *p = l;
+  List *t;
+  while (p != NULL) {
+    t = p->next;
+    free(p);
+    p = t;
+  }
+}
+
+
+int main() {
+  List *list = lst_create();
+  
+  list = lst_insert(list, 20);
+  list = lst_insert(list, 32);
+  list = lst_insert(list, 22);
+  list = lst_insert(list, 99);
+
+  lst_print(list);
+
+  lst_free(list);
+
+  return 0;
+}
---
+++ linked_list/list.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct list List;
+
+// funcão que cria uma lista vazia (NULL)
+List *lst_create();
+
+// função que insere um elemento no fim da lista
+List *lst_insert(List *l, int v);
+
+// função que verifica se a lista está vazia
+int lst_isEmpty(List *l);
+
+// função que imprime os elementos da lista
+void lst_print(List *l);
+
+// função que busca um elemento da lista
+List *lst_search(List *l, int v);
+
+// função que remove um elemento da lista
+void lst_remove(List **l, int v);
+
+// função que libera memória alocada para lista
+void lst_free(List *l);
+
+
